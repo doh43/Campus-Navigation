c7a3a062dc422a04069f56ef8adfffce0240ba6e
Ethan Tiger Wakefield <ewakefi@uwo.ca>
2023-03-25 14:14:13 -0400

    Cleaned up side panel

--- src/main/java/org/example/PoiPanel.java
+++ src/main/java/org/example/PoiPanel.java
@@ -6,8 +6,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 public class PoiPanel extends JPanel implements ActionListener, EditTool {
+    JButton button;
     JButton submit;
     JTextField poiName;
+    JTextField poiId;
     JComboBox poiType;
     JTextField poiRoomNum;
     JTextField poiDesc;
@@ -18,10 +20,18 @@ public class PoiPanel extends JPanel implements ActionListener, EditTool {
 
     PoiPanel() {
         d = Data.getInstance();
-        this.setLayout(null);
+        this.setBounds(0,605,panelWidth,200);
         this.setBackground(Color.GRAY);
+        this.setLayout(null);
+
+        button = new JButton();
+        button.setText("ADD");
+        button.setFocusable(false);
+        button.setBounds(0,0,panelWidth,200);
+        button.addActionListener(this);
 
         poiName = new JTextField("Name");
+        poiId = new JTextField("Id");
         poiRoomNum = new JTextField("Room Num");
         poiDesc = new JTextField("Desc");
 
@@ -31,33 +41,47 @@ public class PoiPanel extends JPanel implements ActionListener, EditTool {
 
         submit = new JButton("Submit");
         submit.setFocusable(false);
+        submit.setBounds(5, 160, 100,20);
         submit.addActionListener(this);
 
         poiName.setBounds(5,10, panelWidth-10, 20);
-        poiType.setBounds(5,40, panelWidth-10, 20);
-        poiRoomNum.setBounds(5,70, panelWidth-10, 20);
-        poiDesc.setBounds(5,100, panelWidth-10, 20);
+        poiId.setBounds(5,40, panelWidth-10, 20);
+        poiType.setBounds(5,70, panelWidth-10, 20);
+        poiRoomNum.setBounds(5,100, panelWidth-10, 20);
+        poiDesc.setBounds(5,130, panelWidth-10, 20);
 
+        this.add(button);
         this.add(poiName);
+        this.add(poiId);
         this.add(poiType);
         this.add(poiRoomNum);
         this.add(poiDesc);
         this.add(submit);
     }
     public void actionPerformed(ActionEvent e) {
-       if (e.getSource() == submit) {
+        if (e.getSource() == button) {
+            if (button.getText().equals("ADD")) {
+                this.setBounds(0, 0, panelWidth, 1000);
+                button.setBounds(0,605, panelWidth,200);
+                button.setText("CLOSE");
+            } else {
+                this.setBounds(0,605,panelWidth,200);
+                button.setBounds(0,0, panelWidth,200);
+                button.setText("ADD");
+            }
+        } else if (e.getSource() == submit) {
             Poi p = new Poi(
                     poiName.getText(),
                     poiType.getSelectedItem().toString(),
-                    0,
+                    Integer.parseInt(poiId.getText()),
                     Integer.parseInt(poiRoomNum.getText()),
                     poiDesc.getText(),
                     "",
                     0,
                     0
-                    );
+            );
             addPoi("mc",0,p.convertJSON());
-       }
+        }
     }
 
     @Override
--- src/main/java/org/example/SidePanel.java
+++ src/main/java/org/example/SidePanel.java
@@ -2,63 +2,81 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 
-public class SidePanel extends JPanel implements ActionListener {
-    JButton button;
+public class SidePanel extends JLayeredPane {
     JPanel poiList;
     JPanel layer;
     JPanel favourites;
     JPanel poiPanel;
+    JPanel selection;
     SidePanel() {
-        setLayout(new GridLayout(4,1));
+        setLayout(null);
         setPreferredSize(new Dimension(200,1000));
 
-        poiList = new PoiListPanel();
-        layer = new LayerPanel();
-        favourites = new FavouritesPanel();
-        poiPanel = new PoiPanel();
+        selection = new JPanel();
+        selection.setBounds(0,0,200,600);
+
+        // PoiListPanel
+        poiList = new JPanel();
+        poiList.setLayout(new BorderLayout());
+        poiList.setBackground(Color.lightGray);
+        poiList.setPreferredSize(new Dimension(200,150));
+
+        JLabel poiSelect = new JLabel("Select POI:");
+        String[] pois = {"Classroom1","Rest2","Lab3","Stair"};
+        JComboBox<String> cb = new JComboBox<>(pois);
 
 
-        button = new JButton();
-        button.setText("ADD");
-        button.setFocusable(false);
-        button.addActionListener(this);
 
-        add(favourites);
-        add(layer);
-        add(poiList);
-        add(button);
+        poiList.add(poiSelect, BorderLayout.NORTH);
+        poiList.add(cb, BorderLayout.CENTER);
+
+        // LayerPanel
+        layer = new JPanel();
+        layer.setLayout(new GridLayout(8,1));
+        layer.setBackground(Color.lightGray);
+        layer.setPreferredSize(new Dimension(200,300));
+
+        JLabel layerSelect = new JLabel("Layers");
+
+        JCheckBox cRoom = new JCheckBox("Classrooms",true);
+        JCheckBox nav = new JCheckBox("Navigation",true);
+        JCheckBox wash = new JCheckBox("Washrooms",true);
+        JCheckBox entry = new JCheckBox("Entry/Exit",true);
+        JCheckBox genL = new JCheckBox("Labs",true);
+        JCheckBox res = new JCheckBox("Restaurants",true);
+        JCheckBox collab = new JCheckBox("Collaborative Rooms",true);
+
+
+        layer.add(layerSelect);
+        layer.add(cRoom);
+        layer.add(nav);
+        layer.add(wash);
+        layer.add(entry);
+        layer.add(genL);
+        layer.add(res);
+        layer.add(collab);
+
+        // Favourites
+        favourites = new JPanel();
+        favourites.setLayout(new BorderLayout());
+        favourites.setBackground(Color.lightGray);
+        favourites.setPreferredSize(new Dimension(200,150));
+
+        JLabel favSelect = new JLabel("Favourites");
+        String[] favs = {"fav1","fav2","fav3","fav4"};
+        JComboBox<String> favBox = new JComboBox<>(favs);
+
+        favourites.add(favSelect, BorderLayout.NORTH);
+        favourites.add(favBox, BorderLayout.CENTER);
+
+        selection.add(poiList);
+        selection.add(layer);
+        selection.add(favourites);
+
+        add(selection, Integer.valueOf(0));
+        add(new PoiPanel(), Integer.valueOf(1));
 
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == button) {
-            if (button.getText().equals("ADD")) {
-                setLayout(new BorderLayout());
-                remove(button);
-                remove(favourites);
-                remove(layer);
-                remove(poiList);
-                add(poiPanel, BorderLayout.NORTH);
-                poiPanel.setPreferredSize(new Dimension(200, 605));
-                add(button, BorderLayout.SOUTH);
-                button.setPreferredSize(new Dimension(200, 200));
-                poiPanel.setVisible(true);
-                button.setText("CLOSE");
-            } else {
-                setLayout(new GridLayout(4, 1));
-                poiPanel.setVisible(false);
-                remove(poiPanel);
-                remove(button);
-                add(favourites);
-                add(layer);
-                add(poiList);
-                add(button);
-                button.setText("ADD");
-            }
-
-        }
-    }
 }
