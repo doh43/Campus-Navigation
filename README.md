498a33ef0bbb010acfa25fb0b88723ca8c57be4c
Azure_bit <tomasgarciash@gmail.com>
2023-04-06 23:36:04 -0400

    Updated the content and graphics of Help.java (also fixed a minor bug). Finished all sections of the README page except for Build and Run Guide. Corrected an error in buildings.json - Tomas Garcia

--- README.md
+++ README.md
@@ -1,3 +1,54 @@
-### Campus Navigation App
+## Campus Navigation App
+This application is a Geographic Information System (GIS) that uses public floor plans to make it easier for people to 
+navigate the interior spaces of a university campus, specifically Western University. It aims to improve the UX of 
+navigating floor plans by allowing users to search for rooms and points of interest in buildings, browse through the 
+maps, hide or display types of points of interest (like classrooms or restaurants) to suit their needs and create and 
+save their own personal points of interest. Additionally, the application has an editing tool to facilitate the creation 
+and editing of map metadata by developers and the creation of custom points of interest by regular users.
+### Required Libraries
+No third party libraries are required for this program. However, it is greatly recommended to check that a recent version of the JRE and JDK are installed.
+### Build Guide
+-> Insert instructions here
+### Running Guide
+-> Insert instructions here
 
-The goal of this project is to develop an application that makes it easier for people to navigate the interior spaces of a university campus, specifically Western University. The application will utilize the public floor plans made by the university to create a GIS (Geographic Information System) software product. The proposed application aims to improve the UX of the floor plans by allowing users to search for rooms and points of interest in buildings, browse through the maps, and create and save their own personal points of interest. Additionally, the application will have an editing tool to facilitate the creation and editing of map metadata by developers.
+### User Guide
+The <b>Landing Page</b> presents a drop-down list of available buildings for the user to select. When a building has been selected, click 'View Map' to confirm the selection
+and proceed to the <b>Map Page</b> to view the building floor maps. The <b>About Page</b> can be accessed from this page to see the application version
+and developer contact information. The 'Back' button can be used to navigate back to the <b>Login Page</b>.
+
+The <b>Map Page</b> contains several features to navigate the given building:
+To search for a specific point of interest (POI) from any floor in the building, input its full name
+or its associated room number into the 'Search Bar' (a POI's id also works) and then click 'Search'.
+If a POI matching your search is found, you will be directed to it and may even be taken to the floor
+where the POI is. To browse POIs from the currently selected floor, use the 'Select POI' dropdown and click on a POI name to select it.
+All POI layers are shown in the map by default, but they can be manually hidden/shown by the user by clicking on its associated button under 'Layers'.
+The 'Favourites' dropdown stores POIs the user has favourited. These will be saved even after the application is closed and can be selected inside the dropdown.
+The 'ADD' button allows admin users to create built-in POIs and regular users to create custom POIs available only to them.
+To move to a new floor, press one of the floor buttons at the bottom of the page. The current floor is shown by the text to the right of the search button.
+To go back to the <b>Landing Page</b>, click on the 'Back' button in the bottom right corner.
+To select a POI from within the Map, click on one of the colored squares.
+
+When the 'ADD' button is clicked, the side panel changes to allow the user to enter details for the new POI (Name, Type, Room Number, Description, and Position).
+The Name, Room Number, and Description are created by inputting text into the given text boxes while the type is chosen from a dropdown menu.
+To set the position of the new POI, click on the 'Set Position' button which temporarily disables all other side panel options. Then, click the spot on
+the map where you want to place your POI using your mouse (this will update the Current Pos text). Once you are happy with the position you chose, click on the button now called
+'Click on Map' again to confirm the position. After filling out all the information, click the Submit button to create the POI. If the POI was created, a 'Success' popup will appear. If any of the POI details has not been filled, the POI will not be created, and you will be asked to try again.
+After adding all the wanted POIs, click the 'Close' button to return to the regular Map mode (this button also works to cancel making a POI).
+
+When a POI is selected, it will display information about itself inside a popup and change its color to <b>Black</b>. Options available to the user will also appear in the popup.
+When a regular user selects a custom POI, they will be allowed to edit it, add it to favourites, or delete it from the map.  If they select a built-in POI, they will only be given the option to add or remove it from their personal favorites list.
+When an admin user selects a built-in POI, they will be able to edit or remove it, but they will not be able to add it to favourites (although that option will be visible in the popup).
+When the 'Edit' button is pressed, the side panel will change to allow the user to input the updated POI information (works like 'ADD'). 
+When the 'Delete' button is pressed, the POI is removed from the map permanently. To protect against accidental deletion, you will be asked to confirm after choosing this option.
+### Account Guide / Edit Mode
+To make a regular user account, simply input your chosen username and password (not hardcoded) and click the register 
+button on the login page. Then click on the login button. Regular users can create, edit, or remove custom points of 
+interest and can also favourite built-in or custom points of interest to create a favourites list.
+Be aware that built-in POIs cannot be made, edited, or removed by regular users but can be manipulated by admin users 
+(developers). To access developer mode, use one of the 
+two admin accounts doh43 or admin both of which have the password cs2212. This will allow you to manipulate built-in 
+points of interest. However, it will not allow you to create custom points of interest or favourite a point of interest.
+
+No separate application is required to access the editing mode as it is built into the Map screen. Whether the editing 
+mode is one for regular users or developers is determined by the login information provided when opening the application. 
--- src/main/java/org/maps/Help.java
+++ src/main/java/org/maps/Help.java
@@ -61,9 +61,9 @@ public class Help extends JFrame implements ActionListener {
 
         // HELP 1
             // Landing Page Navigation
-        String helpPanel1text = "The <b>Landing Page</b> presents a drop-down list of available buildings for the user to select a particular building and click 'View Map' to confirm the selection <br> " +
-                "and proceed to the <b>Map Page</b> to view the building floor maps. The <b>About</b> can be accessed from this page which provides application version <br> " +
-                "and developer contact information. The Back button can be used to navigate back to the <b>Login Page</b>.";
+        String helpPanel1text = "<b>[1]</b> The <b>Landing Page</b> presents a drop-down list of available buildings for the user to select. <b>[2]</b> When a building has been selected, click 'View Map' to confirm the <br>selection " +
+                "and proceed to the <b>Map Page</b> to view the building floor maps. The <b>About Page</b> can be accessed from this page to see the application version " +
+                "and <br>developer contact information. The 'Back' button can be used to navigate back to the <b>Login Page</b>.";
         JTextPane helpPanel1 = new JTextPane();
         helpPanel1.setContentType("text/html");
         helpPanel1.setEditorKit(new HTMLEditorKit());
@@ -82,12 +82,17 @@ public class Help extends JFrame implements ActionListener {
 
         // HELP 2
             // MAP PAGE FEATURES
-        String helpPanel2text = "The Map Page has several features to navigate available points of interests (POIs) and floor maps. Floor selection can be done using the footer tabs [8].<br>" +
-                "The left Side Panel displays a drop-down list of all available POIs for the current floor map [4], a drop-down list of the user-favorite POIs for the current <br> " +
-                "floor map [6], and the POIs displayed in the floor map can be toggled by their layer type using the checkboxes [5]. The search bar [3] can also be used <br>" +
-                "to search for POIs using their name. A POI displayed on the map appears as a colored box [10]. Using the back button [9] will navigate the user back to <br>" +
-                " the landing page where a different building can be selected. The add button [7] can be used by developers and users to create built-in and user-created <br>" +
-                "POIs, respectively. ";
+        String helpPanel2text = "The <b>Map Page</b> contains several features to navigate the given building: <br>" +
+                "<b>[3]</b> To search for a specific point of interest (POI) from any floor in the building, input its full name" +
+                " or its associated room number into the 'Search Bar' <br>(a POI's id also works) and then click 'Search'." +
+                " If a POI matching your search is found, you will be directed to it and may even be taken to the floor " +
+                "where the POI is.<br> <b>[4]</b> To browse POIs from the currently selected floor, use the 'Select POI' dropdown and click on a POI name to select it.<br>" +
+                "<b>[5]</b> All POI layers are shown in the map by default, but they can be manually hidden/shown by the user by clicking on its associated button under 'Layers'.<br>"
+                + "<b>[6]</b> The 'Favourites' dropdown stores POIs the user has favourited. These will be saved even after the application is closed and can be selected inside the dropdown.<br>"
+                + "<b>[7]</b> The 'ADD' button allows admin users to create built-in POIs and regular users to create custom POIs available only to them.<br>"
+                + "<b>[8]</b> To move to a new floor, press one of the floor buttons at the bottom of the page. The current floor is shown by the text to the right of the search button.<br>"
+                + "<b>[9]</b> To go back to the <b>Landing Page</b>, click on the 'Back' button at the bottom right corner.<br>"
+                + "<b>[10]</b> To select a POI from within the Map, click on one of the colored squares.";
         JTextPane helpPanel2 = new JTextPane();
         helpPanel2.setContentType("text/html");
         helpPanel2.setEditorKit(new HTMLEditorKit());
@@ -101,16 +106,17 @@ public class Help extends JFrame implements ActionListener {
             // SET UP HELP2 IMAGE
         JLabel helpImage2 = new JLabel();
         helpImage2.setIcon(new ImageIcon("./data/help/help6.png"));
-        helpImage2.setBounds(100,0, helpViewport.width, helpImage2.getPreferredSize().height);
+        helpImage2.setBounds(50,0, helpViewport.width, helpImage2.getPreferredSize().height);
         tempPanel.add(helpImage2);
 
         // HELP 3
             // ADD FUNCTION
-        String helpPanel3text = "When selecting the add function [7] the Side Panel display is updated to allow the application user to enter POI details (Name, Type, <br>" +
-                "Room Number, Description). The POI location can be set by clicking the Set Position button [11] which temporarily disables side panel features to allow <br>" +
-                " users to ‘Click on Map’. After a location on the visible map has been clicked with the mouse, the location coordinates are updated in the side panel <br>" +
-                " text after ‘Current Pos:’ and selecting the Click on Map button [12] confirms the clicked location. This allows the user to submit [13] their POI <br>" +
-                "details, which displays a confirmation dialog [14], and the user can then close [14] the add function side panel. ";
+        String helpPanel3text = "When the 'ADD' button is clicked, the side panel changes to allow the user to enter details for the new POI (Name, Type, Room Number, Description, and Position).<br>"
+                + "The Name, Room Number, and Description are created by inputting text into the given text boxes while the type is chosen from a dropdown menu.<br>"
+                + "<b>[11]</b> To set the position of the new POI, click on the 'Set Position' button which temporarily disables all other side panel options. Then, click the spot on "
+                + "the map <br>where you want to place your POI using your mouse (this will update the Current Pos text). <b>[12]</b> Once you are happy with the position you chose, click on the <br>button now called"
+                + " 'Click on Map' again to confirm the position. <b>[13]</b> After filling out all the information, click the Submit button to create the POI.<br><b>[14]</b> If the POI was created, a 'Success' popup will appear. If any of the POI details has not been filled, the POI will not be created and you will be asked to try again.<br>"
+                + "<b>[15]</b> After adding all the wanted POIs, click the 'Close' button to return to the regular Map mode (this button also works to cancel making a POI).";
         JTextPane helpPanel3 = new JTextPane();
         helpPanel3.setContentType("text/html");
         helpPanel3.setEditorKit(new HTMLEditorKit());
@@ -124,16 +130,16 @@ public class Help extends JFrame implements ActionListener {
             // SET UP HELP1 IMAGE
         JLabel helpImage3 = new JLabel();
         helpImage3.setIcon(new ImageIcon("./data/help/help7.png"));
-        helpImage3.setBounds(100,0, helpViewport.width, helpImage3.getPreferredSize().height);
+        helpImage3.setBounds(50,0, helpViewport.width, helpImage3.getPreferredSize().height);
         tempPanel.add(helpImage3);
 
         //HELP 4
             // POI INTERACTION
-        String helpPanel4text = "A selected POI can be distinguished by its change in color to black [16]. User-created and built-in POIs can be distinguished by the <br>" +
-                "options available in their POI dialog. User-created POI dialogs [17] provide buttons to ‘Favorite’, ‘Edit’ and ‘Delete’ their POI. Alternatively, <br>" +
-                "built-in POI dialogs [18] will only display a ‘Favorite’ button for users to favorite built-in POIs. Note that all POI dialogs will appear as [17] <br>" +
-                " for developer accounts with all three buttons enabled. The ‘Edit’ button will update the Side Panel with similar features as when the add function <br>" +
-                "is invoked allowing the user to edit and update all properties of the POI. ";
+        String helpPanel4text = "<b>[16]</b> When a POI is selected, it will display information about itself inside a popup and change its color to <b>Black</b>. Options available to the user will also appear in the <br>popup." +
+                " <b>[17]</b> When a regular user selects a custom POI, they will be allowed to edit it, add it to favourites, or delete it from the map. <b>[18]</b> If they select a built-in <br>POI, they will only be given the option to add or remove it from their personal favorites list." +
+                " When an admin user selects a built-in POI, they will be able to edit or <br>remove it but they will not be able to add it to favourites (although that option will be visible in the popup)." +
+                " When the 'Edit' button is pressed, the side panel will <br>change to allow the user to input the updated POI information (works like 'ADD')." +
+                " When the 'Delete' button is pressed, the POI is removed from the map permanently.<br> To protect against accidental deletion, you will be asked to confirm after choosing this option.";
         JTextPane helpPanel4 = new JTextPane();
         helpPanel4.setContentType("text/html");
         helpPanel4.setEditorKit(new HTMLEditorKit());
@@ -147,7 +153,7 @@ public class Help extends JFrame implements ActionListener {
             // SET UP HELP4 IMAGE
         JLabel helpImage4 = new JLabel();
         helpImage4.setIcon(new ImageIcon("./data/help/help4.png"));
-        helpImage4.setBounds(100,0, helpViewport.width, helpImage4.getPreferredSize().height);
+        helpImage4.setBounds(120,0, helpViewport.width, helpImage4.getPreferredSize().height);
         tempPanel.add(helpImage4);
 
 
